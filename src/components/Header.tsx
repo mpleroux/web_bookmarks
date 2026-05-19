336e55643f63267d1d2ad33b447d13706e9f9874
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-05-18 23:06:25 -0400

    Header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,27 +19,32 @@ export default function Header({ onAdd }: HeaderProps) {
   };
 
   return (
-    <header className="mb-6 flex items-center justify-between gap-4 xs:justify-normal">
+    <header className="mb-6 flex items-center justify-between gap-3 xs:justify-normal">
       <h1 className="mb-0 shrink-0 text-lg">Web Bookmarks</h1>
       <div className="hidden shrink grow basis-0 xs:block">
         <SearchBar />
       </div>
-      {user && (
-        <button type="button" onClick={onAdd}>
-          + Add
-        </button>
-      )}
-      {user ? (
-        <button type="button" onClick={handleSignOut}>
-          Sign Out
-        </button>
-      ) : (
-        <button type="button" onClick={() => router.push("/login")}>
-          Sign In
-        </button>
-      )}
-      <div className="hidden xs:block">
-        <ThemeToggle />
+      <div className="flex gap-3">
+        {user && (
+          <button type="button" onClick={onAdd}>
+            + Add
+          </button>
+        )}
+        {user ? (
+          <button type="button" onClick={handleSignOut}>
+            Sign Out
+            <span className="hidden pl-1 sm:inline">
+              ({user.email?.split("@")[0]})
+            </span>
+          </button>
+        ) : (
+          <button type="button" onClick={() => router.push("/login")}>
+            Sign In
+          </button>
+        )}
+        <div className="hidden xs:block">
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
